774f34074de5415f5aa66acfc7e862fcceca64d6
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-06-06 10:03:34 +0530

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+
+import {Outlet,Navigate,Route,Routes,useLocation} from "react-router-dom"
+import { useState } from 'react'
+import { Footer, Navbar } from './components'
+
+
+
+function Layout()
+{
+  const user=true;
+  const loaction=useLocation();
+
+  return user ? <Outlet/>:<Navigate/>
+  
+}
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <main>
+      <Navbar/>
+      <Routes>
+
+        <Route element={<Layout/>}/>
+      </Routes>
+      <Footer/>
+   
+   
+
+    </main>
+  )
+}
+
+export default App
