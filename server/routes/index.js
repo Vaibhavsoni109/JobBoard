ba6c836b5028939d60dd00ba4e8b3d8301375e3a
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-06-16 12:59:34 +0530

    commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 import authRoute from "./authRoutes.js";
 import userRoute from "./userRoutes.js";
-// import companyRoute from "./companiesRoutes.js";
+import companyRoute from "./companiesRoutes.js";
 // import jobRoute from "./jobsRoutes.js";
 
 const router = express.Router();
@@ -11,6 +11,8 @@ const path = "/api-v1/";
 
 router.use(`${path}auth`, authRoute); //api-v1/auth/
 router.use(`${path}users`, userRoute);
+router.use(`${path}companies`, companyRoute);
+
 
 
 export default router;
