094383d5105840d2f2990f224471d58a6c7818b0
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-06-18 11:19:29 +0530

    commit

--- client/src/pages/CompanyProfile.jsx
+++ client/src/pages/CompanyProfile.jsx
@@ -8,6 +8,9 @@ import { FiPhoneCall, FiEdit3, FiUpload } from "react-icons/fi";
 import { Link, useParams } from "react-router-dom";
 import { companies, jobs } from "../utils/data";
 import { CustomButton, JobCard, Loading, TextInput } from "../components";
+import { apiRequest } from "../utils";
+
+import { Login } from "../redux/userSlice";
 
 const CompnayForm = ({ open, setOpen }) => {
   const { user } = useSelector((state) => state.user);
@@ -25,8 +28,46 @@ const CompnayForm = ({ open, setOpen }) => {
   const dispatch = useDispatch();
   const [profileImage, setProfileImage] = useState("");
   const [uploadCv, setUploadCv] = useState("");
+  const [isLoading,setIsLoading]=useState(false)
+  const [errMsg,seterrMsg]=useState({status:false})
 
-  const onSubmit = () => {};
+  const onSubmit =async (data) => {
+    setIsLoading(true)
+    seterrMsg(false)
+
+    const uri=profileImage && (await handleFileUpload(profileImage))
+    const newData=uri ?{...data,profileUrl:uri}:data;
+
+    try {
+      const res=await apiRequest({
+        url:"/companies/update-company",
+        token:user?.token,
+        data:newData,
+        method:"PUT"
+      });
+      setIsLoading(false)
+
+      if(res.status==="failed")
+        {
+          seterrMsg({...res})
+
+        }
+        else{
+          seterrMsg({status:"success",message:res.message})
+          dispatch(Login(data));
+          localStorage.setItem("userinfo",JSON.stringify(data))
+
+          setTimeout(() => {
+            window.location.reload();
+          }, 1500);
+        }
+      
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+    }
+
+  };
 
   const closeModal = () => setOpen(false);
 
@@ -163,8 +204,38 @@ const CompanyProfile = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [openForm, setOpenForm] = useState(false);
 
+  const fetchCompany=async()=>
+    {
+      setIsLoading(true)
+      let id=null;
+      if(params.id && params.id !== undefined)
+        {
+          id=params?.id;
+        }
+        else
+        {
+          id=user?.id;
+        }
+        console.log(id);
+
+        try {
+          const res=await apiRequest({
+            url: "/companies/get-company/"+
+            id,
+            method: 'GET',
+          })
+          setInfo(res?.data)
+          setIsLoading(false)
+        } catch (error) {
+          console.log(error)
+          setIsLoading(false);
+          
+        }
+       
+    }
+
   useEffect(() => {
-    setInfo(companies[parseInt(params?.id) - 1 ]);
+  fetchCompany();
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, []);
 
