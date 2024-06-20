6c23563fc3ebda0aaaffa8fb3907c0e8ad632a92
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-06-20 10:08:03 +0530

    commit

--- client/src/pages/FindJobs.jsx
+++ client/src/pages/FindJobs.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { BiBriefcaseAlt2 } from "react-icons/bi";
 import { BsStars } from "react-icons/bs";
@@ -7,6 +7,7 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import Header from "../components/Header";
 import { experience, jobTypes, jobs } from "../utils/data";
 import { CustomButton, JobCard, ListBox } from "../components";
+import { apiRequest, updateURL } from "../utils";
 
 const FindJobs = () => {
   const [sort, setSort] = useState("Newest");
@@ -14,6 +15,7 @@ const FindJobs = () => {
   const [numPage, setNumPage] = useState(1);
   const [recordCount, setRecordCount] = useState(0);
   const [data, setData] = useState([]);
+  const [expVal,setExpVal]=useState([])
 
   const [searchQuery, setSearchQuery] = useState("");
   const [jobLocation, setJobLocation] = useState("");
@@ -32,10 +34,62 @@ const FindJobs = () => {
       setFilterJobTypes([...filterJobTypes, val]);
     }
   };
+  const fetchJobs=async()=>
+    {
+      setIsFetching(true)
+      const newURL=updateURL({
+        pageNum:page,
+        query:searchQuery,
+        cmpLoc:jobLocation,
+        sort:sort,
+        navigate:navigate,
+        location:location,
+        jType:filterJobTypes,
+        exp:filterExp,
+
+      });
+      try {
+        const res= await apiRequest({
+          url: "/jobs"+newURL,
+          method: "GET",
+        })
+        console.log(res)
+
+        setNumPage(res.noOfPage);
+        setRecordCount(res.totalJobs);
+        setData(res.data) 
+        setIsFetching(false);
+      } catch (error) {
+        setIsFetching(false)
+        console.log(error)
+        
+      }
+    }
 
   const filterExperience = async (e) => {
     setFilterExp(e);
   };
+
+  useEffect(()=>{
+    if(expVal.length>0)
+    {
+      let newExpVal=[];
+      expVal?.map((el)=>
+      {
+        const newEl=el.split("-");
+        newExpVal.push(Number(newEl[0]),Number(newEl[1]));
+
+      });
+      newExpVal.sort((a,b)=>a - b);
+      setFilterExp(`${newExpVal[0]}-${newExpVal[newExpVal.length()]}`);
+    }
+
+    
+  },[expVal])
+
+  useEffect(()=>{
+    fetchJobs();
+  },[sort,filterJobTypes,filterExp,page])
 
   return (
     <div>
@@ -123,9 +177,14 @@ const FindJobs = () => {
           </div>
 
           <div className='w-full flex flex-wrap gap-4'>
-            {jobs.map((job, index) => (
-              <JobCard job={job} key={index} />
-            ))}
+            {data?.map((job, index) => {
+              const newJob={
+                name:job?.company?.name,
+                logo:job?.company?.profileUrl,
+                ...job,
+              }
+             return <JobCard job={newJob} key={index} />
+            })}
           </div>
 
           {numPage > page && !isFetching && (
